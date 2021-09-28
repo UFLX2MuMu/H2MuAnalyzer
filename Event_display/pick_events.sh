e40ad0c6e26622f60f0437953536e2686ed21f21
Xunwu Zuo <zuoxunwu@gmail.com>
2021-09-28 11:13:41 +0200

    push scripts for event display

---
+++ Event_display/pick_events.sh
@@ -0,0 +1,30 @@
+# remember first to: voms-proxy-init -voms cms -rfc
+
+#edmPickEvents.py --runInteractive --output="AOD_WH_2016B" "/SingleMuon/Run2016B-07Aug17_ver2-v1/AOD" WH_2016B_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2016B" "/SingleMuon/Run2016B-17Jul2018_ver2-v1/MINIAOD" WH_2016B_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2016C" "/SingleMuon/Run2016C-17Jul2018-v1/MINIAOD" WH_2016C_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2016E" "/SingleMuon/Run2016E-17Jul2018-v1/MINIAOD" WH_2016E_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2016G" "/SingleMuon/Run2016G-17Jul2018-v1/MINIAOD" WH_2016G_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2016H" "/SingleMuon/Run2016H-17Jul2018-v1/MINIAOD" WH_2016H_cat2_M120130.txt
+
+#edmPickEvents.py --runInteractive --output="WH_2017C" "/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD" WH_2017C_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2017D" "/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD" WH_2017D_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2017F" "/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD" WH_2017F_cat2_M120130.txt
+
+#edmPickEvents.py --runInteractive --output="WH_2018A" "/SingleMuon/Run2018A-17Sep2018-v2/MINIAOD" WH_2018A_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2018B" "/SingleMuon/Run2018B-17Sep2018-v1/MINIAOD" WH_2018B_cat2_M120130.txt
+#edmPickEvents.py --runInteractive --output="WH_2018D_one" "/SingleMuon/Run2018D-PromptReco-v2/MINIAOD" WH_2018D_cat2_M120130.txt 
+edmPickEvents.py --runInteractive --output="AOD_WH_2018D_mu" "/SingleMuon/Run2018D-22Jan2019-v2/AOD" WH_2018D_cat2_M120130.txt
+
+#edmPickEvents.py --runInteractive --output="ZH_2016F" "/SingleMuon/Run2016F-17Jul2018-v1/MINIAOD" ZH_2016F_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="AOD_ZH_2016F" "/SingleMuon/Run2016F-07Aug17-v1/AOD" ZH_2016F_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="ZH_2016G" "/SingleMuon/Run2016G-17Jul2018-v1/MINIAOD" ZH_2016G_cat1_M120130.txt
+
+#edmPickEvents.py --runInteractive --output="ZH_2017C" "/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD" ZH_2017C_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="ZH_2017D" "/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD" ZH_2017D_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="ZH_2017E" "/SingleMuon/Run2017E-31Mar2018-v1/MINIAOD" ZH_2017E_cat1_M120130.txt
+
+#edmPickEvents.py --runInteractive --output="ZH_2018B" "/SingleMuon/Run2018B-17Sep2018-v1/MINIAOD" ZH_2018B_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="ZH_2018C" "/SingleMuon/Run2018C-17Sep2018-v1/MINIAOD" ZH_2018C_cat1_M120130.txt
+#edmPickEvents.py --runInteractive --output="ZH_2018D" "/SingleMuon/Run2018D-PromptReco-v2/MINIAOD" ZH_2018D_cat1_M120130.txt
+
